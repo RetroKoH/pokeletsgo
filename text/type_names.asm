c88a8317336164b30e0fd7c292549b82c35775ec
SonicRetroKingofHarts <cmgaudet2010@gmail.com>
2019-11-16 10:41:27 +0700

    Added Steel, Dark and Fairy types
    Added constants, strings, and type effects for STEEL, DARK, and FAIRY type

--- text/type_names.asm
+++ text/type_names.asm
@@ -6,22 +6,9 @@ TypeNames:
 	dw .Poison
 	dw .Ground
 	dw .Rock
-	dw .Bird
 	dw .Bug
 	dw .Ghost
-
-	dw .Normal
-	dw .Normal
-	dw .Normal
-	dw .Normal
-	dw .Normal
-	dw .Normal
-	dw .Normal
-	dw .Normal
-	dw .Normal
-	dw .Normal
-	dw .Normal
-
+	dw .Steel
 	dw .Fire
 	dw .Water
 	dw .Grass
@@ -29,20 +16,24 @@ TypeNames:
 	dw .Psychic
 	dw .Ice
 	dw .Dragon
+	dw .Dark
+        dw .Fairy
 
 .Normal:   db "NORMAL@"
 .Fighting: db "FIGHTING@"
 .Flying:   db "FLYING@"
 .Poison:   db "POISON@"
+.Ground:   db "GROUND@"
+.Rock:     db "ROCK@"
+.Bug:      db "BUG@"
+.Ghost:    db "GHOST@"
+.Steel:    db "STEEL@"
 .Fire:     db "FIRE@"
 .Water:    db "WATER@"
 .Grass:    db "GRASS@"
 .Electric: db "ELECTRIC@"
 .Psychic:  db "PSYCHIC@"
 .Ice:      db "ICE@"
-.Ground:   db "GROUND@"
-.Rock:     db "ROCK@"
-.Bird:     db "BIRD@"
-.Bug:      db "BUG@"
-.Ghost:    db "GHOST@"
 .Dragon:   db "DRAGON@"
+.Dark:     db "DARK@"
+.Fairy:    db "FAIRY@"
